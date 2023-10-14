a4f9b07e557e411cf8a9b09a12f516d5bcf1a842
akhila c <akhilac.benzer@gmail.com>
2023-10-14 19:34:40 +0530

    formpage commited

--- crud-app/client/src/App.js
+++ crud-app/client/src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
+      <div className="Form">
+        <label>Movie Name :</label>
+        <input type="text" name="movieName"></input>
+        <label>Review :</label>
+        <input type="text" name="review"></input>
+        <button>Add</button>
+      </div>
     </div>
   );
 }
